86fae753933ab395cad9eab469e7dc8b10b55d44
Bahaa Eldeen Mohamed <116496027+Bashbory@users.noreply.github.com>
2023-05-10 14:37:26 +0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html',
+  './src/input.css',
+  './pages/**/*.{html,js}',
+  ],
+  theme: {
+    extend: {
+      visibility: ["group-hover"],
+      fontFamily: {
+        'russo': ['"Lato"', 'sans-serif'],
+        'russo2':['Work Sans', 'sans-serif']
+      
+      },
+    },
+  },
+  plugins: [require("daisyui")],
+  
+
+
+}
+
